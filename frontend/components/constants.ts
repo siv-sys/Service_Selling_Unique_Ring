30c50f4f91da4367471cb4c87452a8c4208df9db
VICHEKA.HAV <VICHEKA.HAV@PNC.LOCAL>
2026-02-27 16:23:32 +0700

    edit constants

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -3,14 +3,14 @@ import { UserAccount } from './types';
 export const ACCOUNTS: UserAccount[] = [
   {
     id: '1',
-    name: 'Jane Doe',
-    email: 'jane.doe@gmail.com',
-    avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&q=80&w=150&h=150',
+    name: 'vicheka Hav',
+    email: 'vichekahav@gmail.com',
+    avatar: 'https://www.vichekahav.online/profile.jpg',
   },
   {
     id: '2',
-    name: 'John Smith',
-    email: 'john.smith@gmail.com',
+    name: 'Siv Sann',
+    email: 'sivsann@gmail.com',
     avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&q=80&w=150&h=150',
   },
 ];
